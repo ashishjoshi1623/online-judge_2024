3875332467ead881a79dde2e45d4449e824cd5fd
ashishjoshi1623 <ashishjoshi1623@gmail.com>
2024-02-03 23:04:59 +0530

    collect data from react-login-form to express

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../public/login.css";
+import axios from "axios";
 
 export default function Login() {
+
+  const [username,setUserName] = useState('');
+  const [password,setPassword] = useState('');
+
+  
+
+  function submitForm(e){
+    e.preventDefault();
+
+    let userData = {
+      username:username,
+      password:password
+    }
+
+    // fetch("http://localhost:3000/api/login",{
+    //   method:'post',
+    //   headers:{
+    //     "Content-Type":"application/json"
+    //   },
+    //   body:JSON.stringify(userData)
+    // }).then(response=>response.json()).then(data=>{
+    //   console.log(data)
+    // })
+
+    // console.log(username +" "+ password)
+    // console.log("clicked");
+
+    axios.post("http://localhost:3000/api/login",{
+      username:username,
+      password: password
+    });
+
+    console.log("clicked");
+  }
+
   return (
     <div className="login-container">
-      <form action="/login" method="post">
+      <form onSubmit={submitForm}>
         <div className="login">
         <div className="userdiv mb-4">
           <label>
             Username:<br />
-            <input className="box-border mt-2" type="text" placeholder=" Enter username" />
+            <input className="box-border mt-2" onChange={(e)=>setUserName(e.target.value)} type="text" placeholder=" Enter username" name="username" />
           </label>
           </div>
           <div className="passdiv mb-4">
           <label>
             Password: <br />
-            <input className="box-border mt-2" type="password" placeholder=" Enter your password" />
+            <input className="box-border mt-2" onChange={(e)=>setPassword(e.target.value)} type="password" placeholder=" Enter your password" name="password" />
           </label>
           </div>
           <div className="loginButton">
-          <input className="btn btn-primary" type="button" value="Login" />
+          <input className="btn btn-primary" type="submit" value="Login" />
         </div>
         </div>
       </form>
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
+import cors from "cors";
+
+
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/",cors(), (req,res)=>{
+    res.send("hello");
+});
 
-app.use(bodyParser.urlencoded({extended : true}));
-
-app.get("/",(req,res)=>{
-    res.send("<h1> Hello </h1>");
+app.post("/api/login",cors(), (req,res)=>{
+    console.log(req.body);
+    res.json({"message":"user info received"});
 });
 
 app.listen(port,()=>{
